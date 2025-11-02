245d590018797c8965cecc13ffbcd2d0af1378e5
wonjong <won72881@naver.com>
2025-11-02 18:30:53 +0900

    code-review

--- src/main/java/com/wjc/codetest/product/model/response/ProductListResponse.java
+++ src/main/java/com/wjc/codetest/product/model/response/ProductListResponse.java
@@ -18,10 +18,10 @@ import java.util.List;
 @Setter
 public class ProductListResponse {
     private List<Product> products;
-    //문제: Controller에서 ProductListResponse에 Entity(Product)를 직접 담아 반환하고 있음.
-    //원인: Service 레이어 결과(Page<Product>)를 변환하지 않고 그대로 Response DTO에 주입함.
-    //개선안: ProductResponse DTO를 별도로 생성해 Entity → DTO 변환 후 반환.
-    //(선택 근거: 엔티티 캡슐화, API 안정성 확보, 직렬화 오류 예방)
+    // 문제: Controller에서 ProductListResponse에 Entity(Product)를 직접 담아 반환하고 있음.
+    // 원인: Service 레이어 결과(Page<Product>)를 변환하지 않고 그대로 Response DTO에 주입함.
+    // 개선안: ProductResponse DTO를 별도로 생성해 Entity → DTO 변환 후 반환.
+    // 선택 근거: 엔티티 캡슐화, API 안정성 확보, 직렬화 오류 예방
     private int totalPages;
     private long totalElements;
     private int page;
